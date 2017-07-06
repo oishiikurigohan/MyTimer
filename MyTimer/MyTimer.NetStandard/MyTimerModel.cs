0a6223d34e75e2dfcdd5d71ac09a175d31765f9c
kurigohan <kurigohan_blog-contact@yahoo.co.jp>
2017-07-06 11:56:02 +0900

    Add project files.

---
+++ MyTimer/MyTimer.NetStandard/MyTimerModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using Reactive.Bindings;
+
+namespace MyTimer
+{
+    public class MyTimerModel
+    {
+        public ReactiveProperty<int> CurrentCount { get; } = new ReactiveProperty<int>(0);
+        private Timer timer;
+
+        public MyTimerModel()
+        {
+            var callBack = new TimerCallback((o) => { CurrentCount.Value++; });
+            timer = new Timer(callBack, null, Timeout.Infinite, Timeout.Infinite);
+        }
+
+        public void Start() => timer.Change(0, 1000);
+        public void Stop() => timer.Change(Timeout.Infinite, Timeout.Infinite);
+    }
+}
---
+++ MyTimer/MyTimer.NetStandard/MyTimerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Reactive.Bindings;
+
+namespace MyTimer
+{
+    public class MyTimerViewModel
+    {
+        public ReactiveProperty<int> CurrentCount { get; }
+        public ReactiveCommand StartCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand StopCommand { get; } = new ReactiveCommand();
+
+        public MyTimerViewModel()
+        {
+            var Model = new MyTimerModel();
+            CurrentCount = Model.CurrentCount;
+            StartCommand.Subscribe(_ => Model.Start());
+            StopCommand.Subscribe(_ => Model.Stop());
+        }
+    }
+}
